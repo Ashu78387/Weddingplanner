c1920c57c3adde934842a11fcce50a008a6dcf2d
Ashu78387 <85445456+Ashu78387@users.noreply.github.com>
2023-08-22 01:11:25 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<div class="container">
+
+<?php @include 'header.php'; ?>
+
+<section class="about">
+
+   <img src="images/about-img.png" alt="">
+   <h3>about us</h3>
+   <p>We provide the best service.We have skilled and experinced staff and well qualified team.</p>
+   <a href="contact.php" class="btn">contact us</a>
+
+</section>
+
+<section class="team">
+
+   <h1 class="heading">our team</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/team-1.jpg" alt="">
+         <h3>ELF</h3>
+         <p>wedding planner</p>
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+      </div>
+
+      <div class="box">
+         <img src="images/team-2.jpg" alt="">
+         <h3>ASHUTOSH</h3>
+         <p>wedding planner</p>
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+      </div>
+
+      <div class="box">
+         <img src="images/team-3.jpg" alt="">
+         <h3>AJU</h3>
+         <p>wedding planner</p>
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+      </div>
+
+      <div class="box">
+         <img src="images/team-4.jpg" alt="">
+         <h3>NAVEEN</h3>
+         <p>wedding planner</p>
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+      </div>
+
+   </div>
+
+</section>
+
+<?php @include 'footer.php'; ?>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
